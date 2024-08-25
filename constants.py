a614dc788aebab54c3707f7a09d6b0f29aa5cafc
turtle4831 <jakethrun@gmail.com>
2024-08-25 14:13:15 -0500

    redid swerve module and deleted faulty drivetrain code (ill fix it later)

--- constants.py
+++ constants.py
@@ -1,137 +1,21 @@
-import math
+#can
+from wpimath._controls._controls.controller import PIDController
 
-from wpimath import units
-from wpimath.geometry import Translation2d
-from wpimath.kinematics import SwerveDrive4Kinematics
-from wpimath.trajectory import TrapezoidProfileRadians
+front_left_drive = 1
+front_right_drive = 2
+back_left_drive = 3
+back_right_drive = 4
 
-from rev import CANSparkMax
+front_left_turn = 5
+front_right_turn = 6
+back_left_turn = 7
+back_right_turn = 8
 
+front_left_enc = 9
+front_right_enc = 10
+back_left_enc = 11
+back_right_enc = 12
 
-class NeoMotorConstants:
-    kFreeSpeedRpm = 5676
-
-
-class DriveConstants:
-    # Driving Parameters - Note that these are not the maximum capable speeds of
-    # the robot, rather the allowed maximum speeds
-    kMaxSpeedMetersPerSecond = 4.8
-    kMaxAngularSpeed = math.tau  # radians per second
-
-    kDirectionSlewRate = 1.2  # radians per second
-    kMagnitudeSlewRate = 1.8  # percent per second (1 = 100%)
-    kRotationalSlewRate = 2.0  # percent per second (1 = 100%)
-
-    # Chassis configuration
-    kTrackWidth = units.inchesToMeters(26.5)
-    # Distance between centers of right and left wheels on robot
-    kWheelBase = units.inchesToMeters(26.5)
-
-    # Distance between front and back wheels on robot
-    kModulePositions = [
-        Translation2d(kWheelBase / 2, kTrackWidth / 2),
-        Translation2d(kWheelBase / 2, -kTrackWidth / 2),
-        Translation2d(-kWheelBase / 2, kTrackWidth / 2),
-        Translation2d(-kWheelBase / 2, -kTrackWidth / 2),
-    ]
-    kDriveKinematics = SwerveDrive4Kinematics(*kModulePositions)
-
-    # Angular offsets of the modules relative to the chassis in radians
-    kFrontLeftChassisAngularOffset = -math.pi / 2
-    kFrontRightChassisAngularOffset = 0
-    kBackLeftChassisAngularOffset = math.pi
-    kBackRightChassisAngularOffset = math.pi / 2
-
-    # SPARK MAX CAN IDs
-    kFrontLeftDrivingCanId = 11
-    kRearLeftDrivingCanId = 13
-    kFrontRightDrivingCanId = 15
-    kRearRightDrivingCanId = 17
-
-    kFrontLeftTurningCanId = 10
-    kRearLeftTurningCanId = 12
-    kFrontRightTurningCanId = 14
-    kRearRightTurningCanId = 16
-
-    TurnEncoderFrontLeft = 1
-    TurnEncoderFrontRight = 2
-    TurnEncoderBackLeft = 3
-    TurnEncoderBackRight = 4
-
-
-    kGyroReversed = False
-
-
-class ModuleConstants:
-    # The MAXSwerve module can be configured with one of three pinion gears: 12T, 13T, or 14T.
-    # This changes the drive speed of the module (a pinion gear with more teeth will result in a
-    # robot that drives faster).
-    kDrivingMotorPinionTeeth = 14
-
-    # Invert the turning encoder, since the output shaft rotates in the opposite direction of
-    # the steering motor in the MAXSwerve Module.
-    kTurningEncoderInverted = True
-
-    # Calculations required for driving motor conversion factors and feed forward
-    kDrivingMotorFreeSpeedRps = NeoMotorConstants.kFreeSpeedRpm / 60
-    kWheelDiameterMeters = 0.0762
-    kWheelCircumferenceMeters = kWheelDiameterMeters * math.pi
-    # 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15 teeth on the bevel pinion
-    kDrivingMotorReduction = (45.0 * 22) / (kDrivingMotorPinionTeeth * 15)
-    kDriveWheelFreeSpeedRps = (
-        kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters
-    ) / kDrivingMotorReduction
-
-    kDrivingEncoderPositionFactor = (
-        kWheelDiameterMeters * math.pi
-    ) / kDrivingMotorReduction  # meters
-    kDrivingEncoderVelocityFactor = (
-        (kWheelDiameterMeters * math.pi) / kDrivingMotorReduction
-    ) / 60.0  # meters per second
-
-    kTurningEncoderPositionFactor = math.tau  # radian
-    kTurningEncoderVelocityFactor = math.tau / 60.0  # radians per second
-
-    kTurningEncoderPositionPIDMinInput = 0  # radian
-    kTurningEncoderPositionPIDMaxInput = kTurningEncoderPositionFactor  # radian
-
-    kDrivingP = 0.04
-    kDrivingI = 0
-    kDrivingD = 0
-    kDrivingFF = 1 / kDriveWheelFreeSpeedRps
-    kDrivingMinOutput = -1
-    kDrivingMaxOutput = 1
-
-    kTurningP = 1
-    kTurningI = 0
-    kTurningD = 0
-    kTurningFF = 0
-    kTurningMinOutput = -1
-    kTurningMaxOutput = 1
-
-    kDrivingMotorIdleMode = CANSparkMax.IdleMode.kBrake
-    kTurningMotorIdleMode = CANSparkMax.IdleMode.kBrake
-
-    kDrivingMotorCurrentLimit = 50  # amp
-    kTurningMotorCurrentLimit = 20  # amp
-
-
-class OIConstants:
-    kDriverControllerPort = 0
-    kDriveDeadband = 0.05
-
-
-class AutoConstants:
-    kMaxSpeedMetersPerSecond = 3
-    kMaxAccelerationMetersPerSecondSquared = 3
-    kMaxAngularSpeedRadiansPerSecond = math.pi
-    kMaxAngularSpeedRadiansPerSecondSquared = math.pi
-
-    kPXController = 1
-    kPYController = 1
-    kPThetaController = 1
-
-    # Constraint for the motion profiled robot angle controller
-    kThetaControllerConstraints = TrapezoidProfileRadians.Constraints(
-        kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared
-    )
+#pid
+swivel_pid = PIDController(0.001,0,0)
+drive_pid = PIDController(0.001,0,0)
